1c27139d40683c472bd40923503242f69d05c52b
LeyKara <leyla.karahan@wiss-edu.ch>
2024-02-02 11:52:34 +0100

    public erstellt

---
+++ Project/Frontend/frontend_companion/src/pages/public.jsx
@@ -0,0 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+import { getPublicContent } from "../functions/getContent";
+
+export async function loader() {
+  const data = await getPublicContent();
+  return { data };
+}
+
+export default function Public() {
+  const { data } = useLoaderData();
+
+  return (
+    <>
+      <h1>Public</h1>
+      <p>Hier sind die Serverdaten:</p>
+      <ol>
+        {data.map((item) => (
+          <li key={item}> {item} </li>
+        ))}
+      </ol>
+    </>
+  );
+}
