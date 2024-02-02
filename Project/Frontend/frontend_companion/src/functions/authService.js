70e1b1230517494ed58148fd5bd1d7304d6a97b6
LeyKara <leyla.karahan@wiss-edu.ch>
2024-02-02 10:46:28 +0100

    authService  erstellt

---
+++ Project/Frontend/frontend_companion/src/functions/authService.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/auth/";
+
+const login = (email, password) => {
+  return axios
+    .post(API_URL + "login", {
+      email,
+      password,
+    })
+    .then((response) => {
+      if (response.data.email) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+  return axios.post(API_URL + "signout").then((response) => {
+    return response.data;
+  });
+};
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user"));
+};
+
+const AuthService = {
+  login,
+  logout,
+  getCurrentUser,
+};
+
+export default AuthService;
