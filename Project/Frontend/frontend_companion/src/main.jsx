e11c1a8d28d16232cfde330e802e845c8ded28f7
LeyKara <leyla.karahan@wiss-edu.ch>
2024-02-02 11:22:30 +0100

    main  angepasst

--- Project/Frontend/frontend_companion/src/main.jsx
+++ Project/Frontend/frontend_companion/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Importiert React und die benötigten Ressourcen
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Importiere Navigation (Seiten, Komponenten)
+import Login from "./pages/login";
+import Private from "./pages/private";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "/private",
+    loader: private_loader,
+    element: <Private />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
